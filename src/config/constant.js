51d407ec0130dca99df7b3c7d796473e4ebe5cd8
aldeiadocafe <aldeiadocafe@aldeiadocafe.com.br>
2026-03-09 19:13:56 -0300

    CBOUTLET

--- src/config/constant.js
+++ src/config/constant.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = import.meta.env.MODE == "development" ? 'http://localhost:5000/api/v1/' : 'cboutlet-backend.vercel.app/api/v1/';
+export const API_BASE_URL = import.meta.env.MODE == "development" ? 'http://localhost:5000/api/v1/' : 'https://cboutlet-backend.vercel.app/api/v1/';
 //export const API_BASE_URL = 'http://192.168.1.11:80/api/v1/';
 export const TAMANHO_PAGINA = 10;
 export const ALDEIA_CNPJ=58694360000110
